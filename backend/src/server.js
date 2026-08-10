1e4211e1505c73c8a15fcb2deb0af35f17b15f44
Harsha <harshahiremath.0000@gmail.com>
2026-08-10 19:22:50 +0530

    Configure backend for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,7 @@ app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 
